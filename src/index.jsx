66c0a4f6e54d55e4b0b7625367a2c714ccaf736c
Nadiya <Kaanya786@gmail.com>
2024-05-20 16:33:51 +0530

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App"
+
+
+ReactDom.render(<App /> , document.getElementById("root"));
+
+
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
